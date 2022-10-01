1b3fdac9d3ccbab7da3da0091065cfdc885e7c65
Ashfak Niloy <ashfakniloy56@gmail.com>
2022-10-02 00:11:46 +0600

    changed padding

--- pages/post-ad/index.js
+++ pages/post-ad/index.js
@@ -6,7 +6,7 @@ import Layout from "../../components/Layout";
 function PostAdPage() {
   return (
     <Layout>
-      <div className="pb-[600px]">
+      <div className="pb-[650px]">
         <h1 className="text-[32px] text-center font-medium">
           Select a country
         </h1>
